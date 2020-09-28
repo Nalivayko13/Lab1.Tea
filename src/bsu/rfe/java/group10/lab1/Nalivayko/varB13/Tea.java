7883e0ea67ecd789766055eb631fdaaf7f8ea421
Polina <nalivayko.popalina@gmail.com>
2020-09-28 20:53:32 +0300

    2 commit

--- src/bsu/rfe/java/group10/lab1/Nalivayko/varB13/Tea.java
+++ src/bsu/rfe/java/group10/lab1/Nalivayko/varB13/Tea.java
@@ -1,4 +1,34 @@
 package bsu.rfe.java.group10.lab1.Nalivayko.varB13;
 
-public class Tea {
+public class Tea extends Food {
+    private String color;
+
+    public Tea(String color) {
+        super("Чаёк");
+        this.color = color;
+    }
+
+    public void consume() {
+        System.out.println(this + " выпит");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean equals(Object arg0) {
+        if (super.equals(arg0)) {
+            if (!(arg0 instanceof Tea)) return false;
+            return color.equals(((Tea)arg0).color);
+        } else
+            return false;
+    }
+
+    public String toString() {
+        return super.toString() + " цвета '" + color.toUpperCase() + "'";
+    }
 }
